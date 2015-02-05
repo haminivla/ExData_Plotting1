aaa912b712c20a568aa471edc02c9fdc2ed885a1
Alvin <alvinmah@outlook.com>
2015-02-05 23:15:18 +0800

    Added R and PNG files for Project

---
+++ plot2.R
@@ -0,0 +1,22 @@
+plot2 <- function() {
+
+	## Read in first 5 rows to get the column classes
+	cols <- read.table("household_power_consumption.txt", sep=";", header=T, nrows=5, stringsAsFactors=F)
+	classes <- sapply(cols, class)
+
+	## Reading the table with column classes specified speeds up the process
+	dat <- read.table("household_power_consumption.txt", sep=";", colClasses=classes, na.strings="?", header=T)
+
+	## Subset data frame based on the required dates and paste date/time columns into a single DateTime column
+	subst <- dat[dat$Date %in% c("1/2/2007", "2/2/2007"),]
+	subst["DateTime"] <- paste(subst$Date, subst$Time)
+
+	## Covert the DateTime column into POSIXlt format and remove the redundant Date/Time columns
+	subst$DateTime <- strptime(subst$DateTime, "%d/%m/%Y %H:%M:%S")
+	subst$Date <- subst$Time <- NULL
+	
+	## Draw the graph with specified parameters and copy the screen output into a PNG file
+	plot(subst$DateTime, subst$Global_active_power, type = "l", xlab="", ylab="Global Active Power (kilowatts)")
+	dev.copy(png, file="plot2.png")
+	dev.off()
+}
---
+++ plot3.R
@@ -0,0 +1,28 @@
+plot3 <- function() {
+
+	## Read in first 5 rows to get the column classes
+	cols <- read.table("household_power_consumption.txt", sep=";", header=T, nrows=5, stringsAsFactors=F)
+	classes <- sapply(cols, class)
+
+	## Reading the table with column classes specified speeds up the process
+	dat <- read.table("household_power_consumption.txt", sep=";", colClasses=classes, na.strings="?", header=T)
+
+	## Subset data frame based on the required dates and paste date/time columns into a single DateTime column
+	subst <- dat[dat$Date %in% c("1/2/2007", "2/2/2007"),]
+	subst["DateTime"] <- paste(subst$Date, subst$Time)
+
+	## Covert the DateTime column into POSIXlt format and remove the redundant Date/Time columns
+	subst$DateTime <- strptime(subst$DateTime, "%d/%m/%Y %H:%M:%S")
+	subst$Date <- subst$Time <- NULL
+	
+	## Draw the graph with specified parameters and copy the screen output into a PNG file
+	with(subst, {
+		plot(DateTime, Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+		points(DateTime, Sub_metering_2, type="l", col="red")
+		points(DateTime, Sub_metering_3, type="l", col="blue")
+		points(DateTime, Sub_metering_3, type="l", col="blue")
+	})
+	legend("topright", lty=1, col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+	dev.copy(png, file="plot3.png")
+	dev.off()
+}
